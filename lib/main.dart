38e2f92ac3d0448aec9fb0834c8a574f4993d2f3
Yuanyu97 <e0322803@u.nus.edu>
2020-05-19 18:08:01 +0800

    Done with project.

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:Expense_Tracker/widgets/transaction_list.dart';
+import 'package:flutter/material.dart';
+
+import './widgets/chart.dart';
+import './widgets/new_transaction.dart';
+import './models/transaction.dart';
+import "./widgets/transaction_list.dart";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.blue,
+          fontFamily: "Quicksand",
+         // errorColor: Colors.red,
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                  fontFamily: "OpenSans",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+                button: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+          appBarTheme: AppBarTheme(
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  title: TextStyle(
+                    fontFamily: "OpenSans",
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+          )),
+      title: "Expenditure Tracker",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    // Transaction(
+    //     id: "t1", title: "New Mum", amount: 69.69, date: DateTime.now()),
+    // Transaction(
+    //     id: "t2", title: "New Dad", amount: 11.11, date: DateTime.now()),
+  ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions
+        .where((tx) => tx.date.isAfter(DateTime.now().subtract(
+              Duration(days: 7),
+            )))
+        .toList();
+  }
+
+  void _addNewTransaction(String title, double amount, DateTime chosenDate) {
+    final newTx = Transaction(
+        amount: amount,
+        date: chosenDate,
+        id: DateTime.now().toString(),
+        title: title);
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+    void _deleteTransaction(String id) {
+      setState(() {
+        _userTransactions.removeWhere((tx) => tx.id == id);
+      });
+    }
+
+  void _startAddNewTx(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) => GestureDetector(
+              onTap: () {},
+              child: NewTransaction(_addNewTransaction),
+              behavior: HitTestBehavior.opaque,
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Expenditure Tracker',
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTx(context),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Chart(_recentTransactions),
+              TransactionList(_userTransactions, _deleteTransaction)
+            ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTx(context),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+import 'package:intl/intl.dart';
+
+class NewTransaction extends StatefulWidget {
+  final Function newTx;
+
+  NewTransaction(this.newTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
+
+  void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
+    final enteredTitle = _titleController.text;
+    final enteredAmounnt = double.parse(_amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmounnt <= 0 || _selectedDate == null) {
+      return;
+    }
+    widget.newTx(enteredTitle, enteredAmounnt, _selectedDate);
+
+    Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((chosenDate) =>
+        chosenDate == null ? null : setState(() => _selectedDate = chosenDate));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: "Insert Title Here!"),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "Insert Amount Here!"),
+              controller: _amountController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitData(),
+            ),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? "No Date Chosen!"
+                          : "Chosen Date: ${DateFormat.yMd().format(_selectedDate)}",
+                    ),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      "Choose Date",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: _presentDatePicker,
+                  ),
+                ],
+              ),
+            ),
+            RaisedButton(
+              child: Text("Add Transaction"),
+              textColor: Theme.of(context).textTheme.button.color,
+              color: Theme.of(context).primaryColor,
+              onPressed: _submitData,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
