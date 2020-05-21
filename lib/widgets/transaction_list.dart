c05944a4b21cad0d643cca6b35409c7876c67e9f
Yuanyu97 <e0322803@u.nus.edu>
2020-05-21 22:53:56 +0800

    Declared some const variables

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -19,7 +19,7 @@ class TransactionList extends StatelessWidget {
                   "No transactions added yet",
                   style: Theme.of(context).textTheme.title,
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 Container(
@@ -36,7 +36,7 @@ class TransactionList extends StatelessWidget {
             itemBuilder: (ctx, index) {
               return Card(
                 elevation: 5,
-                margin: EdgeInsets.symmetric(
+                margin: const EdgeInsets.symmetric(
                   vertical: 8,
                   horizontal: 5,
                 ),
@@ -44,7 +44,7 @@ class TransactionList extends StatelessWidget {
                   leading: CircleAvatar(
                     radius: 30,
                     child: Padding(
-                      padding: EdgeInsets.all(6),
+                      padding: const EdgeInsets.all(6),
                       child: FittedBox(
                           child: Text("\$${transactions[index].amount}")),
                     ),
@@ -60,11 +60,11 @@ class TransactionList extends StatelessWidget {
                       ? FlatButton.icon(
                           textColor: Theme.of(context).errorColor,
                           onPressed: () => deleteTx(transactions[index].id),
-                          icon: Icon(Icons.delete),
-                          label: Text("Delete"),
+                          icon: const Icon(Icons.delete),
+                          label: const Text("Delete"),
                         )
                       : IconButton(
-                          icon: Icon(Icons.delete),
+                          icon: const Icon(Icons.delete),
                           color: Theme.of(context).errorColor,
                           onPressed: () => deleteTx(transactions[index].id),
                         ),
